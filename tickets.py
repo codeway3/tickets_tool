bcf7febf4692c2174a8c6d79917450c61aac877e
codeway3 <codeway3@126.com>
2017-03-14 21:14:24 +0800

    fix a bug and update the paser

--- tickets.py
+++ tickets.py
@@ -2,15 +2,18 @@
 """Train tickets query via command-line.
 
 Usage:
-    tickets [-gdtkz] <from> <to> <date>
+    tickets [-GCDTKZYO] <from> <to> <date>
 
 Options:
     -h,--help   显示帮助菜单
-    -g          高铁
-    -d          动车
-    -t          特快
-    -k          快速
-    -z          直达
+    -G          高铁
+    -C          城际
+    -D          动车
+    -T          特快
+    -K          快速
+    -Z          直达
+    -Y          旅游
+    -O          其他
 
 Example:
     tickets beijing shanghai 2016-08-25
@@ -37,15 +40,16 @@ def colored(color, text):
 
 class TrainCollection(object):
 
-    header = 'train staion time duration business first second softsleep hardsleep hardsit nosit'.split()
+    header = 'train station time duration business first second softsleep hardsleep hardsit nosit'.split()
 
-    def __init__(self, rows):
+    def __init__(self, rows, arguments):
         self.rows = rows
+        self.arguments = arguments
 
     def _get_duration(self, row):
         duration = row.get('lishi').replace(':', 'h') + 'm'
         if duration.startswith('00'):
-            return duration[4:]
+            return duration[3:]
         if duration.startswith('0'):
             return duration[1:]
         return duration
@@ -54,33 +58,39 @@ class TrainCollection(object):
     def trains(self):
         for tmp in self.rows:
             row = tmp['queryLeftNewDTO']
-            train = [
-                row['station_train_code'],
-                '\n'.join([(colored('yellow', '始') if row['start_station_name'] == row['from_station_name'] else colored('blue', '过'))
-                            + ' ' + colored('green', row['from_station_name']),
-                           (colored('purple', '终') if row['end_station_name'] == row['to_station_name'] else colored('blue', '过'))
-                            + ' ' + colored('red', row['to_station_name']),
-                           ' ']),
-                '\n'.join([colored('green', row['start_time']),
-                           colored('red', row['arrive_time'])]),
-                '\n'.join([self._get_duration(row),
-                           '当日到达' if not int(row['day_difference']) else '次日到达']),
-                #商务座
-                row['swz_num'],
-                #一等座
-                row['zy_num'],
-                #二等座
-                row['ze_num'],
-                #软卧
-                row['rw_num'],
-                #硬卧
-                row['yw_num'],
-                #硬座
-                row['yz_num'],
-                #无座
-                row['wz_num']
-            ]
-            yield train
+            row_code = row['station_train_code']
+            if row_code[0].isalpha():
+                check_code = '-' + row_code[0]
+            elif row_code[0].isalnum():
+                check_code = '-O'
+            if self.arguments[check_code]:
+                train = [
+                    row['station_train_code'],
+                    '\n'.join([(colored('yellow', '始') if row['start_station_name'] == row['from_station_name'] else colored('blue', '过'))
+                                + ' ' + colored('green', row['from_station_name']),
+                               (colored('purple', '终') if row['end_station_name'] == row['to_station_name'] else colored('blue', '过'))
+                                + ' ' + colored('red', row['to_station_name']),
+                               ' ']),
+                    '\n'.join([colored('green', row['start_time']),
+                               colored('red', row['arrive_time'])]),
+                    '\n'.join([self._get_duration(row),
+                               '当日到达' if not int(row['day_difference']) else '次日到达']),
+                    #商务座
+                    row['swz_num'],
+                    #一等座
+                    row['zy_num'],
+                    #二等座
+                    row['ze_num'],
+                    #软卧
+                    row['rw_num'],
+                    #硬卧
+                    row['yw_num'],
+                    #硬座
+                    row['yz_num'],
+                    #无座
+                    row['wz_num']
+                ]
+                yield train
 
     def pretty_print(self):
         pt = PrettyTable()
@@ -93,6 +103,7 @@ class TrainCollection(object):
 def cli():
     """command-line interface"""
     arguments = docopt(__doc__)
+    #print(arguments) #验证docopt参数
     from_station = stations.get(arguments['<from>'])
     to_station = stations.get(arguments['<to>'])
     tmp_date = arguments['<date>']
@@ -103,7 +114,7 @@ def cli():
     r = requests.get(url, verify = False)
     #print(r) #验证url返回状态码
     rows = r.json()['data'] #一级解析
-    trains = TrainCollection(rows) #二级解析 创建trains对象
+    trains = TrainCollection(rows, arguments) #二级解析 创建trains对象
     trains.pretty_print() #完全解析 命令行输出查询结果
 
 if __name__ == '__main__':
